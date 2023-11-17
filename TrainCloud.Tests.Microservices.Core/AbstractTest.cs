03c8ee78c4270e1f4de5e400fe8f178093a8c616
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-11-18 00:57:37 +0100

    Add project files.

---
+++ TrainCloud.Tests.Microservices.Core/AbstractTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace TrainCloud.Tests.Microservices.Core;
+
+public class AbstractTest<TProgram> where TProgram : class
+{
+    protected WebApplicationFactory<TProgram> ApplicationFactory { get; init; }
+
+    public AbstractTest()
+    {
+        ApplicationFactory = new WebApplicationFactory<TProgram>().WithWebHostBuilder(builder =>
+        {
+            builder.UseEnvironment("Test");
+            //builder.ConfigureTestServices(services =>
+            //{
+            //    services.RemoveAll(typeof(IImageAnnotatorService));
+            //    services.AddScoped<IImageAnnotatorService, ImageAnnotatorTestService>();
+            //});
+        });
+    }
+
+    protected System.Net.Http.HttpClient Login(string username, string password)
+    {
+        System.Net.Http.HttpClient client = ApplicationFactory.CreateClient();
+
+        return client;
+    }
+}
