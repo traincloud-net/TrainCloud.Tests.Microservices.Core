ce7798eee339656e7382c5d0d8567824efa188c7
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-09-10 17:11:55 +0200

    GetClientAsync(string userName, string password)

--- TrainCloud.Tests.Microservices.Core/Controllers/ApiTest.cs
+++ TrainCloud.Tests.Microservices.Core/Controllers/ApiTest.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Headers;
+using TrainCloud.HttpClient;
+using TrainCloud.Microservices.Identity.Models;
+using TrainCloud.Models;
 
 namespace TrainCloud.Tests.Microservices.Core.Controllers;
 
@@ -32,13 +35,36 @@ public class ApiTest<TProgram> where TProgram : class
     }
 
     /// <summary>
-    /// Creates a HttpClient for the current application. Optional with authorization token
+    /// Creates a HttpClient for the current application with username and password and obtains an Authorization header.
+    /// https://identity.traincloud.dev is the centralized identity service for all test. Local or in Pipeline
+    /// </summary>
+    /// <param name="userName">The user name of the test user</param>
+    /// <param name="password">The password of the Testuser</param>
+    /// <returns></returns>
+    public async Task<System.Net.Http.HttpClient> GetClientAsync(string userName, string password)
+    {
+        System.Net.Http.HttpClient loginClient = new();
+        PostSignInModel postModel = new()
+        {
+            UserName = userName,
+            Password = password
+        };
+
+        TokenModel? model = await loginClient.PostRequestAsync<PostSignInModel, TokenModel>("https://identity.traincloud.dev/Identity/SignIn", postModel);
+
+        System.Net.Http.HttpClient client = GetClient(model?.Token);
+
+        return client;
+    }
+
+    /// <summary>
+    /// Creates a HttpClient for the current application. Optional with authorization token.
     /// </summary>
     /// <param name="token">A JWT, created by TrainCloud.Microservices.Identity</param>
     /// <returns>A instance of HttpClient, for the currently tested application</returns>
-    public HttpClient GetClient(string? token = null)
+    public System.Net.Http.HttpClient GetClient(string? token = null)
     {
-        HttpClient client = ApplicationFactory!.CreateClient();
+        System.Net.Http.HttpClient client = ApplicationFactory!.CreateClient();
 
         if (!string.IsNullOrEmpty(token))
         {
